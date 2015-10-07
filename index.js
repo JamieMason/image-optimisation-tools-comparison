7f7a123bcdd91dd904de09e297a8207acd0b01f3
Jamie Mason <siunmaru@gmail.com>
2015-10-07 18:35:15 +0100

    perf(npm): only share results data on npm, exclude images

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ module.exports = {
     getImagesByName: getImagesByName,
     getImagesByNameByTool: getImagesByNameByTool,
     getImagesByTool: getImagesByTool,
-    getImagesByToolByName: getImagesByToolByName,
-    run: run
+    getImagesByToolByName: getImagesByToolByName
 };
 
 function getImages() {
@@ -27,18 +26,4 @@ function getImagesByTool() {
 
 function getImagesByToolByName() {
     return require('./results/imagesByToolByName.json');
-}
-
-/**
- * @description
- * Regenerate results based on the most recent files in the images directory,
- * gather the file size, quality loss, and score for each file.
- *
- * @return {Promise}
- */
-function run(options) {
-    var data = require('./src/data');
-    return data.gather().then(
-        data.write.bind(null, options.outputDirectory)
-    );
 }
--- runner.js
+++ runner.js
@@ -1,7 +1,19 @@
 'use strict';
 
-var comparison = require('./index');
-
-comparison.run({
+run({
     outputDirectory: __dirname + '/results/'
 });
+
+/**
+ * @description
+ * Regenerate results based on the most recent files in the images directory,
+ * gather the file size, quality loss, and score for each file.
+ *
+ * @return {Promise}
+ */
+function run(options) {
+    var data = require('./src/data');
+    return data.gather().then(
+        data.write.bind(null, options.outputDirectory)
+    );
+}
