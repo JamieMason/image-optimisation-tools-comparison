8dbcea8462e9fbd22a97aed485e60640a7a28bfc
Jamie Mason <siunmaru@gmail.com>
2016-01-03 09:35:05 +0000

    feat(results): update intro text

--- src/app/ResultsComparison/index.js
+++ src/app/ResultsComparison/index.js
@@ -81,7 +81,15 @@ var ResultsComparison = React.createClass({
                     <div className="pack__item intro">
                         <h1>Benchmarks of Image Optimisation Tools</h1>
                         <p>Speeding up images often comes with a cost in picture quality and finding the right balance can be difficult.</p>
-                        <p>Here we compare the percentage file size savings across several image optimisation tools, followed by the percentage of quality lost.</p>
+                        <p>Here we compare the results of several image optimisation tools.</p>
+                        <p className="shields">
+                            <a href="https://twitter.com/fold_left" target="_blank" className="shields__item shields__item--twitter">
+                                <img src="https://img.shields.io/twitter/follow/fold_left.svg?style=social" alt="Follow @fold_left on Twitter" />
+                            </a>
+                            <a href="https://github.com/JamieMason" target="_blank" className="shields__item shields__item--github">
+                                <img src="https://img.shields.io/github/followers/JamieMason.svg?style=social&label=Follow" alt="Follow @fold_left on Twitter" />
+                            </a>
+                        </p>
                     </div>
                     <div className="pack__item">
                         <fieldset className="filters filters--tools">
